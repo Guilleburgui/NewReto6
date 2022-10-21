2aa425d36e57316c971497b01be92df18219ba06
guillermo <guilleburguillos@gmail.com>
2022-10-21 18:04:10 +0200

    primerArchivo

---
+++ index.js
@@ -0,0 +1,29 @@
+function Suma(x,y){
+
+    console.log(x+y);
+}
+
+function Resta(x,y){
+
+    console.log(x-y)
+}
+
+function Producto(x,y){
+
+    console.log(x*y);
+}
+
+function Division(x,y){
+
+    console.log(x / y);
+}
+
+function Cuadrado(x){
+
+    console.log(x * x);
+}
+
+module.exports = {
+
+    Suma, Division, Resta, Producto
+}
---
+++ run.js
@@ -0,0 +1,6 @@
+let myLib = require("./index");
+
+myLib.Resta(10,5);
+myLib.Division(2,7);
+myLib.Suma(4,7);
+myLib.Producto(4,9);
